85edb5576217bb00e0c329d93a1a27ace6d778c0
Udani Amahsa <udanivithanage10@gmail.com>
2026-01-02 11:13:20 +0530

    Update Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; 
+// IMPORT THE PROVIDER
+import { FavoritesProvider } from './context/FavoritesContext'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* WRAP APP IN PROVIDER */}
+    <FavoritesProvider>
+      <App />
+    </FavoritesProvider>
+  </React.StrictMode>
+);
