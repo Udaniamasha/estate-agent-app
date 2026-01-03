c8388c9f4f9ab1972107056a97d5adae53993bd0
Udani Amahsa <udanivithanage10@gmail.com>
2026-01-03 19:17:52 +0530

    fix: Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 // Main pages
 import Home from './pages/Home';
 import PropertyDetails from './pages/PropertyDetails';
